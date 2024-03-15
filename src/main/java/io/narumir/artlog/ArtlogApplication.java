e3a2a3125721db1dfc7e8e85abe0a50f3a419087
narumir <narumir@narumir.io>
2024-03-15 13:46:48 +0000

    initilaize project

---
+++ src/main/java/io/narumir/artlog/ArtlogApplication.java
@@ -0,0 +1,13 @@
+package io.narumir.artlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/narumir/artlog/ArtlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.narumir.artlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
